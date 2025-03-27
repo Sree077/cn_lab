dd23d77a7e1755cbfebefd38fe2cc1fc727b463e
Sreenandan K M <116423469+Sree077@users.noreply.github.com>
2025-03-28 00:54:42 +0530

    Go n Back

---
+++ GoNBack.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+int main() {
+    int total_frames;
+    int window_size;
+    int total_transmissions = 0;
+    
+    srand(time(NULL));
+
+    printf("Enter the Total number of frames: ");
+    scanf("%d", &total_frames);
+    printf("Enter the Window Size: ");
+    scanf("%d", &window_size);
+
+    int current_frame = 1;
+
+    while (current_frame <= total_frames) {
+        int successfully_acked = 0;
+
+        for (int frame = current_frame; 
+             frame < current_frame + window_size && frame <= total_frames; 
+             frame++) 
+        {
+            printf("Sending Frame %d\n", frame);
+            total_transmissions++;
+        }
+
+        for (int frame = current_frame;
+             frame < current_frame + window_size && frame <= total_frames;
+             frame++)
+        {
+            int is_ack_received = rand() % 2;
+
+            if (is_ack_received) {
+                printf("Acknowledgment for Frame %d\n", frame);
+                successfully_acked++;
+            } else {
+                printf("Timeout!! Frame Number %d Not Received\n", frame);
+                printf("Retransmitting Window...\n");
+                sleep(2);
+                break;
+            }
+        }
+
+        printf("\n");
+        current_frame += successfully_acked;
+    }
+
+    printf("Total number of frames sent and resent: %d\n", total_transmissions);
+    return 0;
+}

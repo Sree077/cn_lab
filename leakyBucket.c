e0f4c6b7cad11a49c4f1de29e764e6d7655069e7
Sreenandan K M <116423469+Sree077@users.noreply.github.com>
2025-03-27 23:03:44 +0530

    Leaky Bucket program

---
+++ leakyBucket.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <unistd.h>
+#define BUCKET_LIMIT  10
+#define OUTPUT_RATE  5
+int main(){
+    int bucket_level = 0;
+    int incoming_data = 0;
+    printf("LEAKY BUCKET ALGORITHM SIMULATION:\n");
+    while(1){
+        incoming_data = rand() % 9 + 1;
+        printf("Incoming Data: %d\n", incoming_data);
+
+        if ((incoming_data + bucket_level) <= BUCKET_LIMIT){
+            bucket_level += incoming_data;
+            printf("Bucket Level: %d\n", bucket_level);
+        }
+        else{
+            printf("Bucket overflow! Dropping Data: %d\n", incoming_data);
+        }
+
+        if (bucket_level >= OUTPUT_RATE){
+            bucket_level -= OUTPUT_RATE;
+            printf("Data Sent: %d\n", OUTPUT_RATE);
+            printf("Bucket Level: %d\n", bucket_level);
+        }
+        else{
+            printf("Data Sent: %d\n", bucket_level);
+            bucket_level = 0;
+            printf("Bucket Level: %d\n", bucket_level);
+        }
+        printf("----------------\n");
+        sleep(1);
+
+    }
+    return 0;
+    
+}

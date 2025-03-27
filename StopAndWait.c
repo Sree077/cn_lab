44ac0ac3152b265b22b132df7f3deb119f63b358
Sreenandan K M <116423469+Sree077@users.noreply.github.com>
2025-03-28 00:35:34 +0530

    Stop and Wait

---
+++ StopAndWait.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+int main(){
+    int total_frames;
+    int ack_status;
+    int total_transmissions = 0;
+    int current_frame = 1;
+
+    srand(time(NULL));
+
+    printf("Enter Total Frames: ");
+    scanf("%d", &total_frames);
+
+    while ( current_frame <= total_frames){
+        printf("\nSending Frame: %d\n", current_frame);
+        total_transmissions++;
+
+        ack_status = rand() % 4;
+
+        if (ack_status != 3){
+            printf("ACK Received for Frame: %d\n", current_frame);
+            current_frame++;
+        }
+        else{
+            printf("Timout for frame: %d\n", current_frame);
+            sleep(2);
+            printf("Retransmitting Frame: %d\n", current_frame);
+
+        }
+    }
+    printf("Total Transmissions: %d\n", total_transmissions);
+    return 0;
+}

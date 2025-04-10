f5927eba6cdfaa9dd999081d5a5dffc34fa279f3
Sreenandan K M <116423469+Sree077@users.noreply.github.com>
2025-04-10 23:15:58 +0530

    optimized

--- StopAndWait.c
+++ StopAndWait.c
@@ -1,36 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <unistd.h>
+#include <time.h>
 
 int main(){
-    int total_frames;
+    int total_frame;
     int ack_status;
-    int total_transmissions = 0;
     int current_frame = 1;
+    int total_transmissions = 0;
 
     srand(time(NULL));
 
-    printf("Enter Total Frames: ");
-    scanf("%d", &total_frames);
+    printf("Enter total frames: ");
+    scanf("%d", &total_frame);
 
-    while ( current_frame <= total_frames ){
-        printf("\nSending Frame: %d\n", current_frame);
-        total_transmissions++;
+    
+
+    while (current_frame <= total_frame){
+        printf("Sending Frame: %d\n", current_frame);
+        total_transmissions += 1;
 
         ack_status = rand() % 4;
 
         if (ack_status != 3){
-            printf("ACK Received for Frame: %d\n", current_frame);
-            current_frame++;
+            printf("Acknowledgement Received for frame: %d\n", current_frame);
+            current_frame += 1;
         }
         else{
-            printf("Timout for frame: %d\n", current_frame);
             sleep(2);
-            printf("Retransmitting Frame: %d\n", current_frame);
-
+            printf("Timout for frame: %d\n", current_frame);
+            printf("Retransmitting frame: %d\n", current_frame);
         }
+        sleep(2);
     }
-    printf("Total Transmissions: %d\n", total_transmissions);
+    printf("Total transmisssions: %d\n", total_transmissions);
     return 0;
+
 }
